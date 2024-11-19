84b1efd5e8fcd8ca639db55b332d96a2a627703c
AdithyaVarma28 <adithyavarma5678@gmail.com>
2024-11-19 23:12:56 +0530

    completed server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+from utils.logger_config import logging
+from utils.zone_loader import load_zone
+from handlers.response_builder import build_response
+from config.settings import IP,PORT
+
+def main():
+    zonedata=load_zone()
+    socket_udp=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    socket_udp.bind((IP, PORT))
+    logging.info(f"DNS Server started on {IP}:{PORT}")
+
+    while True:
+        data,address=socket_udp.recvfrom(512)
+        logging.info(f"Received request from {address} with data: {data.hex()}")
+        response=build_response(data,zonedata)
+        logging.info(f"Sending response to {address} with data: {response.hex()}")
+        socket_udp.sendto(response,address)
+        print(f"Received request from {address}")
+        print(f"Sending response to {address}")
+
+if __name__=="__main__":
+    main()
